5072631956c3c5625b406659f2006c5bda307f9f
rtdood@mtu.edu <rtdood@mtu.edu>
2017-06-27 21:29:21 +0000

    fixed overlay buffer issue and modified mmalobj.py to prevent verbose ouput

--- camera3.py
+++ camera3.py
@@ -14,7 +14,9 @@ class Analysis(PiRGBAnalysis):
         self.size = size
         self.x0 = np.array(0)
         self.i = 0
-        self.t = self.camera.timestamp
+        self.t0 = self.camera.timestamp
+        self.xc = 240
+        self.yc = 320
     
     def analyse(self, x):
         x = x[:,:,1]
@@ -30,18 +32,17 @@ class Analysis(PiRGBAnalysis):
                 denom = np.bincount(clust)
                 numerx = np.bincount(clust, xy[:,0])
                 numery = np.bincount(clust, xy[:,1])
-                xc = np.rint((numerx/denom)[0]).astype(np.uint16)
-                yc = np.rint((numery/denom)[0]).astype(np.uint16)
-                print("\r" + str(xc), end="")
-                crosshair[xc, (yc-2):(yc+3), :] = 0xff
-                crosshair[(xc-2):(xc+3), yc, :] = 0xff
-                ol.update(crosshair)
+                self.xc = np.rint((numerx/denom)[0]).astype(np.uint16)
+                self.yc = np.rint((numery/denom)[0]).astype(np.uint16)
+                #crosshair[xc, (yc-2):(yc+3), :] = 0xff
+                #crosshair[(xc-2):(xc+3), yc, :] = 0xff
+                #ol.update(crosshair)
         self.x0 = x
         self.i += 1
-        if self.i % 10 == 0:
-            td = self.camera.timestamp - self.t
-            self.t += td
-            print("\r" + str(10/td), end="")
+        #td = self.camera.timestamp - self.t0
+        #self.t0 += td    
+        #if self.i % 10 == 0:
+        #    print("\r" + str(10/td), end="")
             
 
 rectime = 3
@@ -54,18 +55,32 @@ camera.exposure_mode = 'sports' #sports
 #camera.shutter_speed = 32000
 camera.video_denoise = False
 
-#fullscreen=False, window = (20, 40, 640, 480)
-camera.start_preview()
-ol = camera.add_overlay(crosshair.tobytes(), layer=3, alpha=25) #memoryview tobytes()
+#fullscreen=False, window = (10, 10, 640, 480)
+camera.start_preview(fullscreen=False, window = (10, 10, 960, 720))
+ol = camera.add_overlay(crosshair, layer=3, alpha=25, fullscreen=False, window = (10, 10, 960, 720)) #memoryview tobytes()
 tracker = Analysis(camera)
 camera.start_recording(tracker, format='rgb')
 #camera.start_recording('/home/pi/video2.h264')
-camera.wait_recording(rectime)
+#camera.wait_recording(rectime)
+xc0 = 0
+yc0 = 0
+try:
+    while True:
+        if tracker.xc!=xc0 or tracker.yc!=yc0:
+            crosshair[tracker.xc, (tracker.yc-2):(tracker.yc+3), :] = 0xff
+            crosshair[(tracker.xc-2):(tracker.xc+3), tracker.yc, :] = 0xff
+            ol.update(crosshair)
+        xc0 = tracker.xc
+        yc0 = tracker.yc
+        sleep(.05)
+except KeyboardInterrupt:
+    pass
 camera.remove_overlay(ol)
 camera.stop_recording()
 camera.stop_preview()
-#print(camera.timestamp)
-print("\n%s fps" % ((tracker.i)/rectime))
+print("%s frames" % tracker.i)
+td = (tracker.camera.timestamp - tracker.t0)/1000000
+print("%s fps" % (tracker.i/td))
 
 #omxplayer -o hdmi video2.h264
 #avconv -i video2.h264 -s 640x480 -q:v 1 imgs2/v%04d.jpg
