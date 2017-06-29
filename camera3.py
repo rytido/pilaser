10ff29b0c9af7b80dcf12a354155d5e49dc7c3ac
rtdood@mtu.edu <rtdood@mtu.edu>
2017-06-29 23:15:38 +0000

    simplified object detection and n crosshairs now on the screen at a time

--- camera3.py
+++ camera3.py
@@ -3,7 +3,7 @@ import numpy as np
 from picamera import PiCamera
 from picamera.array import PiRGBAnalysis
 from sklearn.cluster import DBSCAN
-scan = DBSCAN(eps=2, min_samples=3, metric='manhattan', algorithm='kd_tree', leaf_size=40) #euclidean
+scan = DBSCAN(eps=2, min_samples=3, metric='euclidean', algorithm='ball_tree', leaf_size=30) 
 
 class Analysis(PiRGBAnalysis):
     def __init__(self, camera, size=None):
@@ -21,23 +21,13 @@ class Analysis(PiRGBAnalysis):
         d = self.x0-x
         d[self.x0<x] = 0
         xy = np.argwhere(d>12)
-        if xy.shape[0]>2 and xy.shape[0]<1500:
+        if xy.shape[0]>2 and xy.shape[0]<1000:
             clust = scan.fit_predict(xy)
-            ind = clust>-1
+            ind = clust==0
             if ind.any():
-                clust = clust[ind]
-                xy = xy[ind]
-                denom = np.bincount(clust)
-                indd = denom<55
-                if indd.any():
-                    #denom = denom[indd]
-                    #numerx = np.bincount(clust, xy[:,0])[indd]
-                    #numery = np.bincount(clust, xy[:,1])[indd]
-                    #self.xc = np.rint((numerx/denom)[0]).astype(np.uint16)
-                    #self.yc = np.rint((numery/denom)[0]).astype(np.uint16)
-                    clusti = np.nonzero(indd)[0][0]
-                    self.xc = int(xy[:,0][clust==clusti].mean().round(0))
-                    self.yc = int(xy[:,1][clust==clusti].mean().round(0))
+                #xy = xy[ind]
+                self.xc = int(xy[ind,0].mean().round(0))
+                self.yc = int(xy[ind,1].mean().round(0))
         self.x0 = x
         self.i += 1
         #td = self.camera.timestamp - self.t0
@@ -60,20 +50,18 @@ camera.start_recording(tracker, format='rgb')
 
 crosshair = np.zeros((480, 640, 3), dtype=np.uint8)
 ol = camera.add_overlay(crosshair, layer=3, alpha=25, fullscreen=False, window=(0,0,640,480)) #memoryview tobytes()
-xc0 = 0
-yc0 = 0
-c = 0
+
+hist = [[5,5]] * 20
 try:
     while True:
-        if tracker.xc!=xc0 or tracker.yc!=yc0:
-            crosshair[tracker.xc, (tracker.yc-2):(tracker.yc+3), :] = 0xff
-            crosshair[(tracker.xc-2):(tracker.xc+3), tracker.yc, :] = 0xff
+        if tracker.xc!=hist[-1][0] or tracker.yc!=hist[-1][1]:
+            crosshair[tracker.xc, (tracker.yc-3):(tracker.yc+4), :] = 0xff
+            crosshair[(tracker.xc-3):(tracker.xc+4), tracker.yc, :] = 0xff
+            xc,yc = hist.pop(0)
+            crosshair[xc, (yc-3):(yc+4), :] = 0
+            crosshair[(xc-3):(xc+4), yc, :] = 0
             ol.update(crosshair)
-            xc0 = tracker.xc
-            yc0 = tracker.yc
-            c+=1
-            if c % 200 == 0:
-                crosshair = np.zeros((480, 640, 3), dtype=np.uint8)
+            hist.append([tracker.xc,tracker.yc])
         sleep(.05)
 except KeyboardInterrupt:
     pass
