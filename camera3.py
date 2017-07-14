d3ba0f6330d4de3cf91e938803d78d193e180688
rtdood@mtu.edu <rtdood@mtu.edu>
2017-07-14 03:13:08 +0000

    forgot to commit

--- camera3.py
+++ camera3.py
@@ -5,6 +5,9 @@ from picamera.array import PiRGBAnalysis
 from sklearn.cluster import DBSCAN
 scan = DBSCAN(eps=2, min_samples=3, metric='euclidean', algorithm='ball_tree', leaf_size=30) 
 
+def printr(s):
+    print("\r" + s + "      ", end="")
+
 class Analysis(PiRGBAnalysis):
     def __init__(self, camera, size=None):
         self.camera = camera
@@ -28,7 +31,7 @@ class Analysis(PiRGBAnalysis):
         if self.calibrationMode:            
             d = self.x0-x
             d[self.x0<x] = 0
-            if np.any(d>20):
+            if np.any(d>50):
                 self.stableCounter = 0
                 self.bgsum = np.zeros((480, 640), dtype=np.uint16)
             else:
@@ -39,36 +42,36 @@ class Analysis(PiRGBAnalysis):
                     self.stableCounter = 0
                     self.bgsum = np.zeros((480, 640), dtype=np.uint16)
                     self.calibrationMode = False
-                    print("done")
+                    printr("done")
             self.x0 = x
                     
         else: 
             d = self.background-x
             d[self.background<x] = 0        
-            xy = np.argwhere(d>20)
-            if xy.shape[0]>900:
+            xy = np.argwhere(d>60)
+            if xy.shape[0]>999:
                 self.calibrationMode = True
-                print("calibrating")
-            elif xy.shape[0]>2:
+                printr("calibrating")
+            elif xy.shape[0]>4:
                 clust = scan.fit_predict(xy)
                 ind = clust==0
-                if ind.any():
+                if ind.sum()>1: #ind.any()
                     self.xc = xy[ind,0].mean()
                     self.yc = xy[ind,1].mean()
                     xc2 = 2 * self.xc - self.xc0
                     yc2 = 2 * self.yc - self.yc0
-                    self.xp = np.linspace(self.xc0,xc2,10)
-                    self.yp = np.linspace(self.yc0,yc2,10)
+                    #self.xp = np.linspace(self.xc0,xc2,10)
+                    #self.yp = np.linspace(self.yc0,yc2,10)
                     self.xc0 = self.xc
                     self.yc0 = self.yc
+                    printr("%s %s" % (int(self.xc.round()), int(self.yc.round())))
         self.i += 1
         #self.x0 = x
-        #print("\r" + str(10/td), end="")
 
 camera = PiCamera(resolution=(640, 480), framerate=20)
 camera.awb_mode = 'off'
 camera.awb_gains = (1.2, 1.2)
-#camera.iso = 640
+camera.iso = 400 # 400 500 640 800
 camera.color_effects = (128,128)
 camera.exposure_mode = 'sports'
 camera.shutter_speed = 12000
@@ -77,49 +80,35 @@ camera.video_denoise = False
 #fullscreen=False, window=(0,0,640,480)
 camera.start_preview()
 sleep(1)
-print("calibrating")
+printr("calibrating")
 tracker = Analysis(camera)
 camera.start_recording(tracker, format='rgb')
 
-crosshair = np.zeros((480, 640, 3), dtype=np.uint8)
-ol = camera.add_overlay(crosshair, layer=3, alpha=25) 
-#memoryview tobytes()
+#crosshair = np.zeros((480, 640, 3), dtype=np.uint8)
+#ol = camera.add_overlay(crosshair, layer=3, alpha=25) 
 
-hist = [[5,5]] * 30
-#xp0 = 0
-#yp0 = 0
+#hist = [[5,5]] * 30
 try:
-    while True:
-        if not tracker.calibrationMode:
-            xc = int(tracker.xc.round(0))
-            yc = int(tracker.yc.round(0))
-            #xp = tracker.xp.round(0).astype(np.int16)
-            #yp = tracker.yp.round(0).astype(np.int16)
-            if xc!=hist[-1][0] or yc!=hist[-1][1]:
-                crosshair[xc, (yc-3):(yc+4), 1] = 0xff
-                crosshair[(xc-3):(xc+4), yc, 1] = 0xff
-                #try:
-                #    crosshair[xp,yp] = 0xff
-                #except IndexError:
-                #    pass
-                #try:
-                #    crosshair[xp0,yp0] = 0
-                #except IndexError:
-                #    pass
-                ol.update(crosshair)
-                xc0,yc0 = hist.pop(0)
-                crosshair[xc0, (yc0-3):(yc0+4), 1] = 0
-                crosshair[(xc0-3):(xc0+4), yc0, 1] = 0
-                hist.append([xc,yc])
-                #xp0 = xp
-                #yp0 = yp
-        sleep(.05)
+    sleep(9999)
+#    while True:
+#        if not tracker.calibrationMode:
+#            xc = int(tracker.xc.round(0))
+#            yc = int(tracker.yc.round(0))
+#            if xc!=hist[-1][0] or yc!=hist[-1][1]:
+#                crosshair[xc, (yc-3):(yc+4), 1] = 0xff
+#                crosshair[(xc-3):(xc+4), yc, 1] = 0xff
+#                ol.update(crosshair)
+#                xc0,yc0 = hist.pop(0)
+#                crosshair[xc0, (yc0-3):(yc0+4), 1] = 0
+#                crosshair[(xc0-3):(xc0+4), yc0, 1] = 0
+#                hist.append([xc,yc])
+#        sleep(.05)
 except KeyboardInterrupt:
     pass
 
 #camera.start_recording('/home/pi/video2.h264')
 #camera.wait_recording(10)
-camera.remove_overlay(ol)
+#camera.remove_overlay(ol)
 camera.stop_recording()
 camera.stop_preview()
 print("%s frames" % tracker.i)
