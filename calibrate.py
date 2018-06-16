19ba839d3f9984840b183b821ced110ee9e33483
rtdood@mtu.edu <rtdood@mtu.edu>
2018-06-16 15:31:08 -0400

    removed background calibration

--- calibrate.py
+++ calibrate.py
@@ -21,7 +21,6 @@ class Analysis(PiRGBAnalysis):
         self.camera = camera
         self.size = size
         self.z0 = np.array(0)
-        self.calibration_mode = True
         self.stable_counter = 0
         self.background_sum = np.zeros((480, 640), dtype=np.uint16)
         self.background = np.array(0)
@@ -37,52 +36,37 @@ class Analysis(PiRGBAnalysis):
 
     def analyse(self, z):
         z = z[:,:,1]
-        if self.calibration_mode:
-            if np.any((self.z0>z) & (self.z0-z>50)):
-                self.stable_counter = 0
-                self.background_sum = np.zeros((480, 640), dtype=np.uint16)
-            else:
-                self.stable_counter += 1
-                self.background_sum += z
-                if self.stable_counter == 30:
-                    self.background = (self.background_sum/self.stable_counter).round(0).astype(np.uint8)
-                    self.stable_counter = 0
-                    self.background_sum = np.zeros((480, 640), dtype=np.uint16)
-                    self.calibration_mode = False
-                    printr("done")
-            self.z0 = z
+        d = (z>self.z0) & (z-self.z0>80)
+        self.z0 = z
+        xy = np.where(d.ravel())[0]
+        if xy.shape[0]>999:
+            self.laser_xi = 0
+            self.laser_yi = 0
+        elif xy.shape[0]>4:
+            xy = np.transpose(np.unravel_index(xy, d.shape))
+            clust = scan.fit_predict(xy)
+            ind = clust==0
+            if ind.sum()>1:
+                xc = xy[ind,0].mean()
+                yc = xy[ind,1].mean()
+                xint = int(xc.round())
+                yint = int(yc.round())
+                self.campoints.append([xint, yint])
+                self.x_vals.append(self.laser_xi)
+                self.y_vals.append(self.laser_yi)
 
-        else:
-            d = (z>self.background) & (z-self.background>80)
-            xy = np.where(d.ravel())[0]
-            if xy.shape[0]>999:
-                self.calibration_mode = True
-                printr("calibrating")
-            elif xy.shape[0]>4:
-                xy = np.transpose(np.unravel_index(xy, d.shape))
-                clust = scan.fit_predict(xy)
-                ind = clust==0
-                if ind.sum()>1:
-                    xc = xy[ind,0].mean()
-                    yc = xy[ind,1].mean()
-                    xint = int(xc.round())
-                    yint = int(yc.round())
-                    self.campoints.append([xint, yint])
-                    self.x_vals.append(self.laser_xi)
-                    self.y_vals.append(self.laser_yi)
-
-                    if self.laser_xi < self.npoints - 1:
-                        self.laser_xi += 1
-                    elif self.laser_yi < self.npoints - 1:
-                        self.laser_xi = 0
-                        self.laser_yi += 1
-                    else:
-                        self.camera.stop_recording()
-                    laser_x = self.laser_cal_points[self.laser_xi]
-                    laser_y = self.laser_cal_points[self.laser_yi]
-                    open('/dev/spidev0.0', 'wb').write(tohex(laser_x))
-                    open('/dev/spidev0.1', 'wb').write(tohex(laser_y))
-                    printr("%s %s" % (xint, yint))
+                if self.laser_xi < self.npoints - 1:
+                    self.laser_xi += 1
+                elif self.laser_yi < self.npoints - 1:
+                    self.laser_xi = 0
+                    self.laser_yi += 1
+                else:
+                    self.camera.stop_recording()
+                laser_x = self.laser_cal_points[self.laser_xi]
+                laser_y = self.laser_cal_points[self.laser_yi]
+                open('/dev/spidev0.0', 'wb').write(tohex(laser_x))
+                open('/dev/spidev0.1', 'wb').write(tohex(laser_y))
+                printr("%s %s" % (xint, yint))
 
 camera = PiCamera(resolution=(640, 480), framerate=10)
 camera.awb_mode = 'off'
@@ -110,7 +94,7 @@ camera.stop_preview()
 
 
 grid_x, grid_y = np.mgrid[0:640, 0:480]
-
+"""
 x_map_lin = griddata(campoints, x_vals, (grid_x, grid_y), method='linear')
 y_map_lin = griddata(campoints, y_vals, (grid_x, grid_y), method='linear')
 
@@ -121,4 +105,4 @@ y_vals2 = y_map_lin[not_nan]
 
 x_map = griddata(campoints2, x_vals2, (grid_x, grid_y), method='nearest')
 y_map = griddata(campoints2, y_vals2, (grid_x, grid_y), method='nearest')
-
+"""
